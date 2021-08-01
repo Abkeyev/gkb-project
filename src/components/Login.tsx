eba83a792b7281090a60341c761b122da9f8d26e
thepromind <thepromind@gmail.com>
2021-08-01 11:52:47 +0600

    New

---
+++ src/components/Login.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+import "./responsive.css"; 
+
+const Login = () => {
+    return (
+        <div className="">
+            
+           {/* <div className="container">
+               <div className="row">
+                   <div className="page-inner">
+                       <div>
+                           <Link className="back-link" to="/"><img src="images/icons/sr-chevron-square-left.svg" /></Link>
+                       </div>
+                    <h1>
+                        About
+                    </h1>
+                   </div>
+               </div>
+           </div> */}
+
+        </div>
+    );
+}
+export default Login;
