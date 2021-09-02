34d5f24d7e024758d5066b87f3579c249b631a0a
Abkeyev Daniyar <abkeyev@gmail.com>
2021-09-02 21:33:08 +0600

    some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom";
 import "./styles";
 import { AppContext, stores } from "./AppContext";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
   <AppContext.Provider value={stores}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </AppContext.Provider>,
   document.getElementById("root")
 );
