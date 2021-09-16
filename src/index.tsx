fd4213f7fb8e0e3f0abeca8ee8c3b4f40454b484
Abkeyev Daniyar <abkeyev@gmail.com>
2021-09-16 16:27:41 +0600

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,10 @@ import "./styles";
 import MainStore from "./stores/MainStore";
 import RequestStore from "./stores/RequestStore";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App main={MainStore} request={RequestStore} />
-    </BrowserRouter>
+    <App main={MainStore} request={RequestStore} />
   </React.StrictMode>,
   document.getElementById("root")
 );
