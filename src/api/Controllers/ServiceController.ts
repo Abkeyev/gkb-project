ba0ae19e559a8000eb31c6dde24f3a9d7f69cb40
Abkeyev Daniyar <abkeyev@gmail.com>
2021-10-14 03:53:24 +0600

    123

--- src/api/Controllers/ServiceController.ts
+++ src/api/Controllers/ServiceController.ts
@@ -285,7 +285,7 @@ export class ServiceController {
         ...request,
         client: request.client.id,
         is_model_contract: !request.is_model_contract,
-        request_status: !request.is_model_contract ? 2 : 11,
+        request_status: 2,
       },
       {
         baseURL,
